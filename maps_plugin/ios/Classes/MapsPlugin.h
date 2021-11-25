65c5a1c993445cfd5f369496afece63d6964bf35
FerasHamam <feras.i.hamam@gmail.com>
2021-11-25 11:48:02 +0200

    created the plugin!

---
+++ maps_plugin/ios/Classes/MapsPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface MapsPlugin : NSObject<FlutterPlugin>
+@end
---
+++ maps_plugin/ios/Classes/MapsPlugin.m
@@ -0,0 +1,20 @@
+#import "MapsPlugin.h"
+
+@implementation MapsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"maps_plugin"
+            binaryMessenger:[registrar messenger]];
+  MapsPlugin* instance = [[MapsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
